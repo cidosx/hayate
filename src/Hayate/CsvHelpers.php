1cf2f0db4624826b1f2ebf8c94e4d511a1e8e384
锦阳 <ginnerpeace@live.com>
2016-09-13 14:27:42 +0800

    优化

--- src/Hayate/CsvHelpers.php
+++ src/Hayate/CsvHelpers.php
@@ -2,6 +2,11 @@
 
 namespace Hayate;
 
+use RuntimeException;
+
+/**
+ * 处理csv的辅助函数
+ */
 class CsvHelpers {
 
 	/** end of line */
@@ -16,7 +21,6 @@ class CsvHelpers {
 	/** tab分隔符 */
 	CONST DELIMITER_TAB = '	';
 
-
 	/**
 	 * 保存为csv文件
 	 * @author gjy
@@ -25,27 +29,27 @@ class CsvHelpers {
 	 * @param  array $data
 	 * @return boolean
 	 */
-	public static function storage($filePathName, array $data, $overwrite = FALSE) {
+	public static function store($filePathName, array $data, $overwrite = FALSE) {
+
+		if (strrchr($filePathName, '.csv') !== '.csv') {
+			throw new RuntimeException('The file extension must be .csv');
+		}
 
 		if (is_file($filePathName) && !$overwrite) {
-			// TODO throw exception
-			return FALSE;
+			throw new RuntimeException('File already exists.');
 		}
 
 		if (empty($data['rows'])) {
-			return FALSE;
+			throw new RuntimeException('Missing required data for rows.');
 		}
-
-		touch($filePathName);
-		$filePathName = realpath($filePathName);
 
 		$fp = fopen($filePathName, 'w');
 
 		// 添加bom头 EFBBBF 为utf8 bom
-		fwrite($fp, pack('H*','EFBBBF'));
+		fwrite($fp, pack('H*', 'EFBBBF'));
 
 		// 自定义一个数组header, 与cell分开处理
-		if (! empty($data['header'])) {
+		if (!empty($data['header'])) {
 			fwrite($fp, self::ENCLOSURE
 				. join(self::ENCLOSURE . self::DELIMITER . self::ENCLOSURE, $data['header'])
 				. self::ENCLOSURE
@@ -55,30 +59,15 @@ class CsvHelpers {
 		// cell中需要判断是否为长数字
 		foreach ($data['rows'] as $row) {
 			if (is_array($row)) {
-				$contents = '';
-
-				foreach ($row as $cell) {
-					if (is_numeric($cell)) {
-						// 增加一个tab防止数字被转换成科学计数
-						$contents .= self::ENCLOSURE . $cell . '	' . self::ENCLOSURE . self::DELIMITER;
-					}
-					else {
-						// 转义内容中的引号
-						$contents .= self::ENCLOSURE . str_replace('"', '""', $cell) . self::ENCLOSURE . self::DELIMITER;
-					}
-				}
-
-				fwrite($fp, rtrim($contents, self::DELIMITER) . self::EOL);
-			}
-			else {
+				fwrite($fp, rtrim(self::each_row_cell($row), self::DELIMITER) . self::EOL);
+			} else {
 				fwrite($fp, $row . self::EOL);
 			}
 		}
 
 		fclose($fp);
-		return $filePathName;
+		return realpath($filePathName);
 	}
-
 
 	/**
 	 * 下载文件
@@ -90,5 +79,76 @@ class CsvHelpers {
 	 */
 	public static function download($filename, array $data) {
 		// 类似的逻辑, 但结果变为输出到浏览器
+
+		if (empty($data['rows'])) {
+			throw new RuntimeException('Missing required data for rows.');
+		}
+
+		// 自动加上文件后缀
+		$filename = trim($filename);
+		if (strrchr($filename, '.csv') !== '.csv') {
+			$filename .= '.csv';
+		}
+
+		ob_end_clean();
+		ob_start();
+
+		header('Content-Type:text/csv;charset=utf8');
+		header('Content-Disposition:attachment;filename=' . $filename);
+		header('Cache-Control:must-revalidate,post-check=0,pre-check=0');
+		header('Expires:Mon, 26 Jul 1997 05:00:00 GMT');
+		header('Pragma:public');
+
+		// 添加bom头 EFBBBF 为utf8 bom
+		echo pack('H*', 'EFBBBF');
+
+		// 自定义一个数组header, 与cell分开处理
+		if (!empty($data['header'])) {
+			echo self::ENCLOSURE
+			. join(self::ENCLOSURE . self::DELIMITER . self::ENCLOSURE, $data['header'])
+			. self::ENCLOSURE
+			. self::EOL;
+		}
+
+		// cell中需要判断是否为长数字
+		foreach ($data['rows'] as $row) {
+			if (is_array($row)) {
+				echo rtrim(self::each_row_cell($row), self::DELIMITER) . self::EOL;
+			} else {
+				echo $row . self::EOL;
+			}
+		}
+
+		// 输出缓冲区的内容到浏览器
+		ob_end_flush();
+		exit;
+	}
+
+	/**
+	 * 格式化每行的数据
+	 * @author gjy
+	 *
+	 * @param  array $rowData
+	 * @return string
+	 */
+	private static function each_row_cell(array $rowData) {
+		$contents = '';
+
+		foreach ($rowData as $cell) {
+			if (is_numeric($cell)) {
+				// 增加一个tab防止数字被转换成科学计数
+				$contents .= self::ENCLOSURE . $cell . self::DELIMITER_TAB . self::ENCLOSURE . self::DELIMITER;
+			} else {
+				// 转义内容中的引号
+				$contents .= self::ENCLOSURE .
+				str_replace(
+					self::ENCLOSURE,
+					self::ENCLOSURE . self::ENCLOSURE,
+					$cell
+				) . self::ENCLOSURE . self::DELIMITER;
+			}
+		}
+
+		return $contents;
 	}
 }
