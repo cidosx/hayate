eb10a0adea3ca4107871c7aa9b57e758b9da9468
gjy <ginnerpeace@live.com>
2017-05-01 16:25:03 +0800

    optimze: php format

--- src/Hayate/ArrayHelpers.php
+++ src/Hayate/ArrayHelpers.php
@@ -5,241 +5,258 @@ namespace Hayate;
 /**
  * 自定义数组辅助函数
  */
-class ArrayHelpers {
+class ArrayHelpers
+{
 
-	/**
-	 * 性能更高的数组去重
-	 * 解决 array_unique 使用快排算法处理大数组时的性能损耗问题
-	 * @author gjy
-	 *
-	 * @param  array $arrData
-	 * @return array
-	 */
-	public static function unique(array $arrData = array()) {
-		/**
-		 * 去null后两次翻转
-		 * merge用来修复数组index
-		 */
-		return array_merge(array_flip(array_flip(array_filter($arrData))));
-	}
+    /**
+     * 性能更高的数组去重
+     * 解决 array_unique 使用快排算法处理大数组时的性能损耗问题
+     * @author gjy
+     *
+     * @param  array $arrData
+     * @return array
+     */
+    public static function unique(array $arrData = array(), $filtered = false)
+    {
+        if (!$filtered) {
+            $arrData = array_filter($arrData);
+        }
 
-	/**
-	 * 数据排序, 根据参数执行升序或降序
-	 * 依赖 _quick_sort_desc函数
-	 *
-	 * @param  array $array
-	 * @param  string $key
-	 * @param  boolean $asc
-	 * @return array
-	 */
-	public static function quick_sort(array $array = array(), $key = '', $asc = FALSE) {
-		return $asc ? array_reverse(self::_quick_sort_desc($array, $key)) : self::_quick_sort_desc($array, $key);
-	}
+        /**
+         * 去null后两次翻转
+         * merge用来修复数组index
+         */
+        return array_merge(array_flip(array_flip($arrData)));
+    }
 
-	/**
-	 * 根据键值降序排序
-	 *
-	 * @param  array $array
-	 * @param  string $key
-	 * @return array
-	 */
-	private static function _quick_sort_desc(array $array = array(), $key = '') {
-		if (empty($array) || count($array) <= 1) {
-			return $array;
-		}
+    /**
+     * 数据排序, 根据参数执行升序或降序
+     * 只适用于自然序列的数组
+     *
+     * 依赖 _quick_sort_desc函数
+     *
+     * @param  array $array
+     * @param  string $key
+     * @param  boolean $asc
+     * @return array
+     */
+    public static function quick_sort(array $array = array(), $key = '', $asc = false)
+    {
+        return $asc ? array_reverse(self::_quick_sort_desc($array, $key)) : self::_quick_sort_desc($array, $key);
+    }
 
-		$end = count($array) - 1;
-		$middle = intval($end / 2) + 1;
+    /**
+     * 根据键值降序排序
+     *
+     * @param  array $array
+     * @param  string $key
+     * @return array
+     */
+    private static function _quick_sort_desc(array $array = array(), $key = '')
+    {
+        if (empty($array) || count($array) <= 1) {
+            return $array;
+        }
 
-		$r1 = self::_quick_sort_desc(array_slice($array, 0, $middle), $key);
-		$r2 = self::_quick_sort_desc(array_slice($array, $middle, $end - $middle + 1), $key);
+        $end = count($array) - 1;
+        $middle = intval($end / 2) + 1;
 
-		$r = array();
-		$idx1 = $idx2 = 0;
-		$len1 = count($r1);
-		$len2 = count($r2);
+        $r1 = self::_quick_sort_desc(array_slice($array, 0, $middle), $key);
+        $r2 = self::_quick_sort_desc(array_slice($array, $middle, $end - $middle + 1), $key);
 
-		while (true) {
-			if ($idx1 >= $len1) {
-				break;
-			}
+        $r = array();
+        $idx1 = $idx2 = 0;
+        $len1 = count($r1);
+        $len2 = count($r2);
 
-			if ($idx2 >= $len2) {
-				break;
-			}
+        while (true) {
+            if ($idx1 >= $len1) {
+                break;
+            }
 
-			if ($r1[$idx1][$key] >= $r2[$idx2][$key]) {
-				array_push($r, $r1[$idx1]);
-				$idx1++;
-			} else {
-				array_push($r, $r2[$idx2]);
-				$idx2++;
-			}
-		}
+            if ($idx2 >= $len2) {
+                break;
+            }
 
-		if ($idx1 < $len1) {
-			$r = array_merge($r, array_slice($r1, $idx1, count($r1) - $idx1));
-		}
+            if ($r1[$idx1][$key] >= $r2[$idx2][$key]) {
+                array_push($r, $r1[$idx1]);
+                $idx1++;
+            } else {
+                array_push($r, $r2[$idx2]);
+                $idx2++;
+            }
+        }
 
-		if ($idx2 < $len2) {
-			$r = array_merge($r, array_slice($r2, $idx2, count($r2) - $idx2));
-		}
+        if ($idx1 < $len1) {
+            $r = array_merge($r, array_slice($r1, $idx1, count($r1) - $idx1));
+        }
 
-		return $r;
-	}
+        if ($idx2 < $len2) {
+            $r = array_merge($r, array_slice($r2, $idx2, count($r2) - $idx2));
+        }
 
-	/**
-	 * array_map扩展, 实现多维数组处理
-	 * @author gjy
-	 *
-	 * @param  string $filter # 处理数组值的函数名称, 如有自定义的函数务必写在 class{} 外面 !
-	 * @param  array $data
-	 * @return array
-	 */
-	public static function array_map_recursive($filter, $data) {
-		$return = array();
-		foreach ($data as $key => $val) {
-			$return[$key] = is_array($val) ? self::array_map_recursive($filter, $val) : $filter($val);
-		}
-		return $return;
-	}
+        return $r;
+    }
 
-	/**
-	 * 随机取数组中一个值
-	 * @author gjy
-	 *
-	 * @param  array $array
-	 * @return array | mixed
-	 */
-	public static function random_value(array $array) {
-		return is_array($array) ? $array[array_rand($array)] : $array;
-	}
+    /**
+     * array_map扩展, 实现多维数组处理
+     * @author gjy
+     *
+     * @param  string $filter # 处理数组值的函数名称, 如有自定义的函数务必写在 class{} 外面 !
+     * @param  array $data
+     * @return array
+     */
+    public static function array_map_recursive($filter, $data)
+    {
+        $return = array();
+        foreach ($data as $key => $val) {
+            $return[$key] = is_array($val) ? self::array_map_recursive($filter, $val) : $filter($val);
+        }
+        return $return;
+    }
 
-	/**
-	 * fetch some item
-	 * @author gjy
-	 *
-	 * @param  mixed $items
-	 * @param  array $array
-	 * @param  mixed $default
-	 * @return array
-	 */
-	public static function fetch_items(array $array, $items, $default = NULL) {
-		if (is_numeric($items) || is_string($items)) {
-			return isset($array[$items]) ? $array[$items] : $default;
-		}
+    /**
+     * 随机取数组中一个值
+     * @author gjy
+     *
+     * @param  array $array
+     * @return array | mixed
+     */
+    public static function random_value(array $array)
+    {
+        return is_array($array) ? $array[array_rand($array)] : $array;
+    }
 
-		if (!is_array($items)) {
-			return $default;
-		}
+    /**
+     * fetch some item
+     * @author gjy
+     *
+     * @param  mixed $items
+     * @param  array $array
+     * @param  mixed $default
+     * @return array
+     */
+    public static function fetch_items(array $array, $items, $default = null)
+    {
+        if (is_numeric($items) || is_string($items)) {
+            return isset($array[$items]) ? $array[$items] : $default;
+        }
 
-		$return = array();
-		foreach ($items as $item) {
-			$return[$item] = isset($array[$item]) ? $array[$item] : $default;
-		}
+        if (!is_array($items)) {
+            return $default;
+        }
 
-		return $return;
-	}
+        $return = array();
+        foreach ($items as $item) {
+            $return[$item] = isset($array[$item]) ? $array[$item] : $default;
+        }
 
-	/**
-	 * 二维数组里fetch, 保留键名
-	 * @author gjy
-	 *
-	 * @param  array $array
-	 * @param  array $items
-	 * @param  mixed $default
-	 * @return array
-	 */
-	public static function fetch_more(array $array, $items, $default = NULL) {
-		$return = array();
+        return $return;
+    }
 
-		foreach ($array as $key => $val) {
-			$return[$key] = array();
-			foreach ($items as $v) {
-				$return[$key][$v] = isset($val[$v]) ? $val[$v] : $default;
-			}
-		}
+    /**
+     * 二维数组里fetch, 保留键名
+     * @author gjy
+     *
+     * @param  array $array
+     * @param  array $items
+     * @param  mixed $default
+     * @return array
+     */
+    public static function fetch_more(array $array, $items, $default = null)
+    {
+        $return = array();
 
-		return $return;
-	}
+        foreach ($array as $key => $val) {
+            $return[$key] = array();
+            foreach ($items as $v) {
+                $return[$key][$v] = isset($val[$v]) ? $val[$v] : $default;
+            }
+        }
 
-	/**
-	 * 可深度fetch, 结果是一维数组 (laravel里偷来的2333...
-	 *
-	 * @param  array $array
-	 * @param  string $key
-	 * @return array
-	 */
-	public static function fetch(array $array, $key) {
-		foreach (explode('.', $key) as $segment) {
-			$results = array();
+        return $return;
+    }
 
-			foreach ($array as $value) {
-				if (array_key_exists($segment, $value = (array) $value)) {
-					$results[] = $value[$segment];
-				}
-			}
+    /**
+     * 可深度fetch, 结果是一维数组 (laravel里偷来的2333...
+     *
+     * @param  array $array
+     * @param  string $key
+     * @return array
+     */
+    public static function fetch(array $array, $key)
+    {
+        foreach (explode('.', $key) as $segment) {
+            $results = array();
 
-			$array = array_values($results);
-		}
+            foreach ($array as $value) {
+                if (array_key_exists($segment, $value = (array) $value)) {
+                    $results[] = $value[$segment];
+                }
+            }
 
-		return array_values($results);
-	}
+            $array = array_values($results);
+        }
 
-	/**
-	 * 使用传入的key当作外层array的index
-	 * @author gjy
-	 *
-	 * @param  array $array
-	 * @param  string | integer $key
-	 * @return array
-	 */
-	public static function change_index(array $array, $key) {
-		$check = reset($array);
-		if (!isset($check[$key])) {
-			return array();
-		}
+        return array_values($results);
+    }
 
-		$results = array();
-		foreach ($array as $v) {
-			if (isset($results[$v[$key]])) {
-				continue;
-			}
+    /**
+     * 使用传入的key当作外层array的index
+     * @author gjy
+     *
+     * @param  array $array
+     * @param  string | integer $key
+     * @return array
+     */
+    public static function change_index(array $array, $key)
+    {
+        $check = reset($array);
+        if (!isset($check[$key])) {
+            return array();
+        }
 
-			$results[$v[$key]] = $v;
-		}
+        $results = array();
+        foreach ($array as $v) {
+            if (isset($results[$v[$key]])) {
+                continue;
+            }
 
-		return $results;
-	}
+            $results[$v[$key]] = $v;
+        }
 
-	/**
-	 * fetch array return map
-	 * @author gjy
-	 *
-	 * @param  array $array
-	 * @param  string $keyValue # ex: item_id.title
-	 * @return array
-	 */
-	public static function fetch_map(array $array, $keyValue) {
-		$check = reset($array);
-		$kv = explode('.', $keyValue);
-		if (count($kv) !== 2 || !isset($check[$kv[0]], $check[$kv[1]])) {
-			return array();
-		}
+        return $results;
+    }
 
-		$indexKey = $kv[0];
-		$valueKey = $kv[1];
+    /**
+     * fetch array return map
+     * @author gjy
+     *
+     * @param  array $array
+     * @param  string $keyValue # ex: item_id.title
+     * @return array
+     */
+    public static function fetch_map(array $array, $keyValue)
+    {
+        $check = reset($array);
+        $kv = explode('.', $keyValue);
+        if (count($kv) !== 2 || !isset($check[$kv[0]], $check[$kv[1]])) {
+            return array();
+        }
 
-		$results = array();
-		foreach ($array as $v) {
-			if (isset($results[$v[$indexKey]])) {
-				continue;
-			}
+        $indexKey = $kv[0];
+        $valueKey = $kv[1];
 
-			$results[$v[$indexKey]] = $v[$valueKey];
-		}
+        $results = array();
+        foreach ($array as $v) {
+            if (isset($results[$v[$indexKey]])) {
+                continue;
+            }
 
-		return $results;
-	}
+            $results[$v[$indexKey]] = $v[$valueKey];
+        }
+
+        return $results;
+    }
 
 }
